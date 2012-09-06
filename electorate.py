cd90111330a63c84f711a867e459514a15d6380b
Thierry Carrez <thierry@openstack.org>
2012-09-06 10:35:14 +0200

    New match modes

--- electorate.py
+++ electorate.py
@@ -18,12 +18,14 @@ class Member(object):
 
 class Committer(object):
     def __init__(self):
+        self.fullname = None
         self.emails = set()
         self.projects = []
         self.member = None
 
     def add(self, row, project):
-        for email in row[1:]:
+        self.fullname = row[1]
+        for email in row[2:]:
             self.emails.add(email)
         self.projects.append(project)
 
@@ -46,33 +48,67 @@ with open(membersfile, 'rb') as f:
     for row in reader:
         members[row[2]]=Member(row)
 
-voters = {}
+email_matches = {}
+fullname_matches = {}
+reorderedname_matches = {}
+lastname_matches = {}
 missing = {}
+
 for lpid,committer in committers.iteritems():
-    for email in committer.emails:
-        if email in members:
-            voters[lpid] = committer
-            voters[lpid].associate(members[email])
+    # email match
+    for email, member in members.iteritems():
+        if email in committer.emails:
+            email_matches[lpid] = committer
+            email_matches[lpid].associate(member)
             break
     else:
-        missing[lpid] = committer
+        # fullname match
+        for email, member in members.iteritems():
+            if (committer.fullname == "%s %s" % (
+                    member.first_name, member.last_name)):
+                fullname_matches[lpid] = committer
+                fullname_matches[lpid].associate(member)
+                break
+        else:
+        # reordered name match
+            for email, member in members.iteritems():
+                if (committer.fullname.find(member.last_name) != -1 and
+                    committer.fullname.find(member.first_name) != -1):
+                    reorderedname_matches[lpid] = committer
+                    reorderedname_matches[lpid].associate(member)
+                    break
+            else:
+                # Last name only match
+                for email, member in members.iteritems():
+                    if (committer.fullname.find(member.last_name) != -1):
+                        lastname_matches[lpid] = committer
+                        lastname_matches[lpid].associate(member)
+                        break
+                else:
+                    # Add to missing
+                    missing[lpid] = committer
 
-if action == 'csv':
-    writer = csv.writer(sys.stdout)
-    for lpid, committer in voters.iteritems():
-        row = [lpid, committer.member.email]
-        row.extend(committer.projects)
-        writer.writerow(row)
-
-if action == 'names':
-    for lpid, committer in voters.iteritems():
-        print "%s %s (%s)" % (committer.member.first_name,
-                              committer.member.last_name, lpid)
-
-if action == 'emails':
-    for lpid, committer in voters.iteritems():
-        print committer.member.email
-
-if action == 'missing':
-    for lpid, committer in missing.iteritems():
-        print lpid, list(committer.emails), committer.projects
+print "== Email matches =="
+for lpid, committer in email_matches.iteritems():
+    print "%s %s" % (lpid, committer.member.email)
+print
+print "== Full name matches =="
+for lpid, committer in fullname_matches.iteritems():
+    print "%s %s %s" % (lpid, committer.member.email, committer.emails)
+print
+print "== Reordered name matches =="
+for lpid, committer in reorderedname_matches.iteritems():
+    print "%s %s [%s %s = %s]" % (
+        lpid, committer.member.email, committer.member.first_name,
+        committer.member.last_name, committer.fullname)
+print
+print "== Last name matches =="
+for lpid, committer in lastname_matches.iteritems():
+    print "%s %s [%s %s = %s ?]" % (
+        lpid, committer.member.email, committer.member.first_name,
+        committer.member.last_name, committer.fullname)
+print
+print "== Missing matches =="
+for lpid, committer in missing.iteritems():
+    print "%s %s %s" % (
+        lpid, committer.fullname, list(committer.emails))
