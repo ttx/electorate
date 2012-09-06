2e498f16ef9d5b6a7484d3de5cd0c665543f31af
Thierry Carrez <thierry@openstack.org>
2012-09-06 15:07:39 +0200

    More workable output

--- electorate.py
+++ electorate.py
@@ -1,14 +1,14 @@
 import csv
 import os
 import sys
+import StringIO
 
-if len(sys.argv) < 3:
-    print "Usage: %s [csv|names|emails|missing] membersfile commitfile ..." % sys.argv[0]
+if len(sys.argv) < 2:
+    print "Usage: %s membersfile commitfile ..." % sys.argv[0]
     sys.exit(1)
 
-action = sys.argv[1]
-membersfile = sys.argv[2]
-committersfiles = sys.argv[3:]
+membersfile = sys.argv[1]
+committersfiles = sys.argv[2:]
 
 class Member(object):
     def __init__(self, row):
@@ -48,67 +48,64 @@ with open(membersfile, 'rb') as f:
     for row in reader:
         members[row[2]]=Member(row)
 
-email_matches = {}
-fullname_matches = {}
-reorderedname_matches = {}
-lastname_matches = {}
 missing = {}
+pure_output = StringIO.StringIO()
+impure_output = StringIO.StringIO()
 
 for lpid,committer in committers.iteritems():
-    # email match
-    for email, member in members.iteritems():
-        if email in committer.emails:
-            email_matches[lpid] = committer
-            email_matches[lpid].associate(member)
-            break
-    else:
-        # fullname match
-        for email, member in members.iteritems():
-            if (committer.fullname == "%s %s" % (
-                    member.first_name, member.last_name)):
-                fullname_matches[lpid] = committer
-                fullname_matches[lpid].associate(member)
-                break
-        else:
-        # reordered name match
-            for email, member in members.iteritems():
-                if (committer.fullname.find(member.last_name) != -1 and
-                    committer.fullname.find(member.first_name) != -1):
-                    reorderedname_matches[lpid] = committer
-                    reorderedname_matches[lpid].associate(member)
-                    break
-            else:
-                # Last name only match
-                for email, member in members.iteritems():
-                    if (committer.fullname.find(member.last_name) != -1):
-                        lastname_matches[lpid] = committer
-                        lastname_matches[lpid].associate(member)
-                        break
-                else:
-                    # Add to missing
-                    missing[lpid] = committer
+    email_matches = []
+    fullname_matches = []
+    reorderedname_matches = []
+    lastname_matches = []
+    match_found = False
 
-print "== Email matches =="
-for lpid, committer in email_matches.iteritems():
-    print "%s %s" % (lpid, committer.member.email)
+    for email, member in members.iteritems():
+        # email matches
+        if email in committer.emails:
+            email_matches.append(member)
+        # full name matches
+        if (committer.fullname == "%s %s" % (
+                member.first_name, member.last_name)):
+            fullname_matches.append(member)
+        # reordered name match
+        if (committer.fullname.find(member.last_name) != -1 and
+                committer.fullname.find(member.first_name) != -1):
+            reorderedname_matches.append(member)
+        # Last name only match
+        if (committer.fullname.find(member.last_name) != -1):
+            lastname_matches.append(member)
+
+    prefix= "%s (%s)" % (lpid, committer.fullname)
+    if len(email_matches) == 1:
+        print >>pure_output, "%s %s [email match]" % (
+                              prefix, email_matches[0].email)
+        continue
+    if len(email_matches) > 1:
+        print >>impure_output, "%s [DUPLICATE email matches:" % prefix,
+        for member in email_matches:
+            print >>impure_output, member.email,
+        print >>impure_output, "]"
+        continue
+    if len(fullname_matches) == 1:
+        print >>pure_output, "%s %s [fullname match]" % (
+                              prefix, fullname_matches[0].email)
+        continue
+    if len(fullname_matches) > 1:
+        print >>impure_output, "%s [DUPLICATE fullname matches:" % prefix,
+        for member in fullname_matches:
+            print >>impure_output, member.email,
+        print >>impure_output, "]"
+        continue
+    if reorderedname_matches or lastname_matches:
+        print >>impure_output, "%s [FUZZY matches with:" % prefix,
+        for member in reorderedname_matches + lastname_matches:
+            print >>impure_output, "%s %s (%s)," % (
+                         member.first_name, member.last_name, member.email),
+        print >>impure_output, "]"
+        continue
+    print >>impure_output, "%s MISSING, apparently not a member" % prefix
+print "PURE matches:"
+print pure_output.getvalue()
 print
-print "== Full name matches =="
-for lpid, committer in fullname_matches.iteritems():
-    print "%s %s %s" % (lpid, committer.member.email, committer.emails)
-print
-print "== Reordered name matches =="
-for lpid, committer in reorderedname_matches.iteritems():
-    print "%s %s [%s %s = %s]" % (
-        lpid, committer.member.email, committer.member.first_name,
-        committer.member.last_name, committer.fullname)
-print
-print "== Last name matches =="
-for lpid, committer in lastname_matches.iteritems():
-    print "%s %s [%s %s = %s ?]" % (
-        lpid, committer.member.email, committer.member.first_name,
-        committer.member.last_name, committer.fullname)
-print
-print "== Missing matches =="
-for lpid, committer in missing.iteritems():
-    print "%s %s %s" % (
-        lpid, committer.fullname, list(committer.emails))
+print "IMPURE matches:"
+print impure_output.getvalue()
